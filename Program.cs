f943d5dac62913d0164717e836db4b680585313c
bokusik <44327086+bokusik@users.noreply.github.com>
2020-05-12 17:27:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using System.ComponentModel.Design;
+
+namespace TestApp
+{
+    abstract class Test
+    {
+        public void Print()
+        {
+            Console.WriteLine("\nDigDes!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myType = typeof(TestApp.Test);
+
+            MethodInfo[] myArrayMethodInfo = myType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            PrintArrayMethodInfo(myArrayMethodInfo);
+
+            Type TypeHandleType = typeof(System.RuntimeTypeHandle);
+
+            MethodInfo myAllocateMethodInfo = TypeHandleType.GetMethod("Allocate", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+            Type myAllocateParam = myType.UnderlyingSystemType;
+
+            object[] myAllocateParams = new object[] { myAllocateParam };
+
+            Test exampleTest = myAllocateMethodInfo.Invoke(null, myAllocateParams) as Test;
+
+            exampleTest.Print();
+        }
+
+        static void PrintArrayMethodInfo(MethodInfo[] myArrayMethodInfo)
+        {
+            for (int i = 0; i < myArrayMethodInfo.Length; i++)
+                Console.WriteLine("\nThe name of the method is {0}.", myArrayMethodInfo[i].Name);
+        }
+    }
+}
